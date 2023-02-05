61c5a0750d8029655e20447fc59162ccc526fd56
ThostDk <thomas.osterhammel@gmail.com>
2023-02-05 12:48:56 +0100

    Feature:  You can input how many you want to see

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,6 @@
 import { ObserversModule } from '@angular/cdk/observers';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Pokemon } from 'src/app/models/pokemon.model';
 //import { }
 import { PokemonCatalogueService } from 'src/app/services/pokemon-catalogue.service';
@@ -10,14 +11,29 @@ import { PokemonCatalogueService } from 'src/app/services/pokemon-catalogue.serv
 })
 export class PokemonListComponent implements OnInit {
   pokemons: any[] = [];
- 
+  pokemonCount: number = 20;
   constructor(private pokeCatalogueService: PokemonCatalogueService) {}
 
   // get the api data and run through each of the pokemons
   // then get & push their inner data by name to the pokemon array
   ngOnInit(): void {
+    this.renderPokemons(this.pokemonCount);
+  }
+  
+  findPokemonCountSubmit(f:NgForm) {
+    const age = parseInt(f.value.age)
+    console.log(age)
+    if (isNaN(age)) {
+      console.log("input is not a number!")
+    } else {
+      this.renderPokemons(age)
+    }
+  }
+  renderPokemons = (count: number): void =>{
+    this.pokemons = [];
+    console.log("count: "+count)
     this.pokeCatalogueService
-      .fetchPokeApiData(200)
+      .fetchPokeApiData(count)
       .subscribe((apiResponse: any) => {
          apiResponse.results.forEach((result: { name: string }) => {
           this.pokeCatalogueService
@@ -28,6 +44,5 @@ export class PokemonListComponent implements OnInit {
         });
       });
   }
-  
   
 }
